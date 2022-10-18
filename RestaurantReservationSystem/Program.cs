324614d01540bf08eb2c457350fe08b7ccf152bd
Eduardo Peraza <eduardo.peraza.vindas@gmail.com>
2022-10-17 21:57:39 -0600

    instrucciones e initial commit

--- RestaurantReservationSystem/Program.cs
+++ RestaurantReservationSystem/Program.cs
@@ -1,5 +1,12 @@
 ﻿using System;
 
+/*Instrucciones:
+ * 1. Validar si es usuario existe o si se debe registrar
+ * 2. Dar bievenida a usuario existente
+ * 3. Debe repetirse hasta que se llenen las 10 reservaciones
+ */
+
+
 namespace RestaurantReservationSystem
 {
     internal class Program
