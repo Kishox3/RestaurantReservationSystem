dbe0d1b8f2975a1a9f09f56d1dc287d87ee560d6
Eduardo Peraza <eduardo.peraza.vindas@gmail.com>
2022-10-18 08:04:20 -0600

    se agrega limite de registros y verificando de usuarios ya registrados

--- RestaurantReservationSystem/Program.cs
+++ RestaurantReservationSystem/Program.cs
@@ -14,30 +14,50 @@ namespace RestaurantReservationSystem
         static void Main(string[] args)
         {
             //   0       1   2   3   4   5   6   7   8   9
-            string[] registeredUsers = new string[10] { "EDUARDO", "", "", "", "", "", "", "", "", "" };
+            string[] registeredUsers = new string[10] { "EDUARDO", "-table 2", "-table 3", "-table 4", "-table 5", "-table 6", "-table 7", "-table 8", "-table 9", "-table 10" };
             int arrayCurrentIndex = 1;
             string reservationCheck;
             string newRegister;
             string checkRegister;
             Console.WriteLine("Welcome to best restaurant in the World!");
-            while (true)
+            while (arrayCurrentIndex < 10)
             {
                 Console.WriteLine("Are you already make your reservation? Y/N:");
                 reservationCheck = Convert.ToString(Console.ReadLine());
 
                 if (reservationCheck == "n" || reservationCheck == "N")
-                {
+                {   
+                    
                     Console.WriteLine("Type your name for making your reservation");
                     newRegister = Console.ReadLine();
                     registeredUsers[arrayCurrentIndex] = newRegister.ToUpper();
                     arrayCurrentIndex++;
+                    Console.WriteLine("------------------------ \n Actual registration list: \n ------------------------");
+                    for (int i = 0; i < registeredUsers.Length; i++)
+                    {
+                        Console.WriteLine(registeredUsers[i]);
+                    }
+                                  
                 }
                 else if (reservationCheck == "y" || reservationCheck == "Y")
                 {
-                    Console.WriteLine("Type your full name for you reservation");
-                    checkRegister = Convert.ToString(Console.ReadLine());
+                    Console.WriteLine("Type your name for check you reservation");
+                    checkRegister = Console.ReadLine();
+                    for (int i = 0; i < registeredUsers.Length; i++)
+                    {
+                        if (registeredUsers[i].ToUpper() == checkRegister.ToUpper())
+                        {
+                            Console.WriteLine(registeredUsers[i] + " you already have the table #" + (i+1));
+                        }
+                    };
                 }
              }
+            Console.WriteLine("------------------------ \n There's no more tables available: \n ------------------------");
+            for (int i = 0; i < registeredUsers.Length; i++)
+            {
+                Console.WriteLine(registeredUsers[i]);
+            }
+            Console.WriteLine("------------------------ \n Thank you for use our service \n ------------------------");
         }
     }
 }
